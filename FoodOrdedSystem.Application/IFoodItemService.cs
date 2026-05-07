8e9805f45788c4796a097f150c82cf05f1765e92
john.vong <john.vong@winson-group.com>
2026-05-07 18:25:14 +0800

    feat:Added application layer and update README

---
+++ FoodOrdedSystem.Application/IFoodItemService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoodOrdedSystem.Domain;
+
+namespace FoodOrdedSystem.Application
+{
+    public interface IFoodItemService
+    {
+        Task<List<FoodItem?>> GetFoodItemListAsync();
+        Task<bool> AddFoodItemAsync(FoodItem item);
+        Task<FoodItem?> UpdateFoodItemAsync(FoodItem item);
+        Task DeleteFoodItem(int fid);
+    }
+}
